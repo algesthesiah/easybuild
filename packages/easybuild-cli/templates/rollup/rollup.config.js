8377d17ddd4dc1ec038542dbb6d1a01feb454c13
luleo <541877028@qq.com>
2022-04-12 16:11:12 +0800

    feat: update eb cli

--- packages/easybuild-cli/templates/rollup/rollup.config.js
+++ packages/easybuild-cli/templates/rollup/rollup.config.js
@@ -1,19 +1,45 @@
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs'
-import { preserveShebangs } from 'rollup-plugin-preserve-shebangs'
 import { terser } from 'rollup-plugin-terser'
+import resolve from '@rollup/plugin-node-resolve' // 帮助寻找 node_modules 里的包
+import postcss from 'rollup-plugin-postcss'
+import babel from '@rollup/plugin-babel'
 
 const config = [
   {
     input: './index.ts',
     output: [
       {
-        file: 'bin/index.js',
-        format: 'cjs',
+        file: 'lib/index.js', // 通用模块
+        format: 'umd',
+      },
+      {
+        file: 'es/index.js', // es6 模块
+        format: 'es',
       },
     ],
     external: [id => id.includes('@babel/runtime')],
-    plugins: [preserveShebangs(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), terser()],
+    plugins: [
+      resolve(),
+      commonjs(),
+      postcss(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      babel({
+        babelHelpers: 'runtime',
+        exclude: ['node_modules/**'],
+        pluginOptions: ['@babel/plugin-transform-runtime'],
+        presetsOptions: [
+          [
+            '@babel/env',
+            {
+              useBuiltIns: 'usage',
+              corejs: { version: 2 },
+            },
+          ],
+        ],
+      }),
+      terser(),
+    ],
   },
 ]
 
